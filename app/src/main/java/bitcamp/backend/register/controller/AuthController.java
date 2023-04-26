7d3ce1b219cfd47eb62e8f7f4b4acc45780f5c1d
whtndgus <whtndgus12@gmail.com>
2023-04-26 18:24:52 +0900

    04월 25일 작업 내용 업로드

--- app/src/main/java/bitcamp/backend/register/controller/AuthController.java
+++ app/src/main/java/bitcamp/backend/register/controller/AuthController.java
@@ -70,7 +70,7 @@ public class AuthController {
   }
 
   @PostMapping("/doctorLogin")
-  public Object doctorLogin(String id, String password, HttpSession session) {
+  public Object doctorLogin(String id, String password, HttpSession session) { 
 
 
     Member member = null;
@@ -82,7 +82,7 @@ public class AuthController {
       session.setAttribute("mycheck", false);
       for(int i = 0; i < member.licenses.size(); i++) {
         if(member.licenses.get(i).getLicenseOx()) {
-          return new RestResult().setStatus(RestStatus.SUCCESS);    
+          return new RestResult().setStatus(RestStatus.SUCCESS);
         }
       }
       return new RestResult().setStatus(RestStatus.FAILURE).setData("licenseNONE");
