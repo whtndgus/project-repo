8a588aef2ef31566da4327b68fc25882621240e0
hyunwookimkim <chef_klye@naver.com>
2023-04-26 17:31:14 +0900

    크롭이미지 수정및 언디파인 이미지 업로드 추가

--- app/src/main/resources/static/register/doctor-boardlist.js
+++ app/src/main/resources/static/register/doctor-boardlist.js
@@ -23,22 +23,21 @@ fetch(`http://175.106.99.31/auth/user`, {
   .then((response) => response.json())
   .then((data) => {
     if (data.status == "success") {
-      //옥동자 이름
+      //사용자 이름
       document.querySelector("#username").innerHTML = data.data.name;
-      //옥동자 이미지
+      //사용자 이미지
       const preImageContainer = document.querySelector("#pre-userimg");
-      const phoUrl =
-        "http://uyaxhfqyqnwh16694929.cdn.ntruss.com/member-img/" +
-        data.data.phoUrl +
-        "?type=f&w=36&h=36&quality=100&anilimit=24";
+      let phoUrl = "";
+      if (data.data.phoUrl != "undefined") {
+        phoUrl =
+          "http://uyaxhfqyqnwh16694929.cdn.ntruss.com/member-img/" +
+          data.data.phoUrl +
+          "?type=f&w=36&h=36&quality=100&anilimit=24";
+      } else {
+        phoUrl = "../assets/img/default_profile.png";
+      }
       const phoType = data.data.phoType;
       const phoName = data.data.phoName;
-
-      // 기존의 이미지 요소 삭제
-      const oldImg = document.querySelector("#userimg");
-      if (oldImg) {
-        oldImg.remove();
-      }
 
       // 새로운 이미지 요소 생성 및 추가
       const newImg = document.createElement("img");
@@ -140,7 +139,10 @@ function reflesh(string) {
           let obj = {
             answer: board.fedcount,
             no: board.no,
-            title: board.title.length > 45 ? board.title.substring(0,43)+". . ." : board.title,
+            title:
+              board.title.length > 45
+                ? board.title.substring(0, 43) + ". . ."
+                : board.title,
             warn: "경고",
             warnLevel: str,
             writer:
