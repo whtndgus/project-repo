f2c2c2bf2511017d7eea343d3777139c774afb88
whtndgus <whtndgus12@gmail.com>
2023-04-27 16:52:40 +0900

    04월 25일 작업 내용 업로드

--- app/src/main/resources/static/register/AnA-list.js
+++ app/src/main/resources/static/register/AnA-list.js
@@ -5,6 +5,49 @@ const tooltipList = [...tooltipTriggerList].map(
   (tooltipTriggerEl) => new bootstrap.Tooltip(tooltipTriggerEl)
 );
 
+fetch(`http://175.106.99.31/auth/user`, {
+  method: "GET",
+})
+  .then((response) => response.json())
+  .then((data) => {
+    if (data.status == "success") {
+      //사용자 이름
+      document.querySelector("#username").innerHTML = data.data.name;
+      //사용자 이미지
+      const preImageContainer = document.querySelector("#pre-userimg");
+      let phoUrl = "";
+      if (data.data.phoUrl != "undefined") {
+        phoUrl =
+          "http://uyaxhfqyqnwh16694929.cdn.ntruss.com/member-img/" +
+          data.data.phoUrl +
+          "?type=f&w=36&h=36&quality=100&anilimit=24";
+      } else {
+        phoUrl = "../assets/img/default_profile.png";
+      }
+      const phoType = data.data.phoType;
+      const phoName = data.data.phoName;
+
+      // 새로운 이미지 요소 생성 및 추가
+      const newImg = document.createElement("img");
+      newImg.setAttribute("id", "userimg");
+      newImg.setAttribute("src", phoUrl);
+      newImg.setAttribute("alt", phoName);
+      newImg.setAttribute("style", "width:36px; border-radius:50%");
+      preImageContainer.appendChild(newImg);
+      return data.data;
+    } else {
+      location.href = "index.html";
+    }
+    return data.data;
+  })
+  .then((user) => {
+    if (user.admin) {
+      // myno = user.no
+    } else {
+      location.href = "index.html";
+    }
+  });
+
 fetch(`http://175.106.99.31/qna`)
   .then((response) => response.json())
   .then((data) => {
